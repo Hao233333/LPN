75150a13b28996b3dc65136c2fddf62def482a6c
Hao <hao.qin@griffithuni.edu.au>
2023-05-12 12:09:42 +1000

    Initial Commit

---
+++ LPN/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LPN
+//
+//  Created by 秦昊 on 12/5/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
